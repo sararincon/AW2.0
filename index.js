c5c52200d7ec2150bf46b20b6ff00e4292ee7036
Sara Rincón Gonzalez <sararincon@MacBook-Air-de-Sara-2.local>
2022-05-23 23:31:51 -0400

    WD

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require("express");
 const { Pool } = require("pg");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const { v4: uuidv4 } = require("uuid");
 
 const config = {
@@ -23,99 +23,109 @@ class MyError extends Error {
   }
 }
 
+function errorHandler(res, err) {
+  console.log(err.code, err.message);
+  if (typeof err.code !== "number")
+    res.status(500).send({ message: err.message });
+  res.status(err.code).send({ message: err.message });
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//  Read usuarios
+// Leyendo usuarios registrados
 app.get("/usuarios", async (req, res) => {
   try {
-    const SQLQuery= {
+    const SQLQuery = {
       rowMode: "array",
-      text:  "SELECT * FROM usuarios",
-  }
+      text: "SELECT * FROM usuarios",
+    };
     const data = await pool.query(SQLQuery);
+    // console.log(data.rows);
     res.status(200).json({ count: data.rowCount, items: data.rows });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    errorHandler(res, error);
   }
 });
 
-//Create usuario
+//Creando un usuario
 app.post("/usuario", async (req, res) => {
-  try{
-  const uid = uuidv4().slice(0, 6);
-  const { nombre, rut, curso, nivel } = req.body;
-  const SQLQuery = {
-  text: "INSERT INTO usuarios ( nombre, rut, curso, nivel, uuid) VALUES ($1, $2, $3, $4, $5) RETURNING *;",
-  values: [nombre, rut, curso, nivel, uid],
-  }
-  const data = await pool.query(SQLQuery);
-  console.log(data.rows[0]);
-  res.status(200).json({ message: "Estudiante creado correctamente" });
-}
-catch (error) {
-  res.status(500).json({ error: error.message })
+  try {
+    const uid = uuidv4().slice(0, 6);
+    const { nombre, rut, curso, nivel } = req.body;
+    if (!nombre || !rut || !curso || !nivel)
+      throw new MyError(400, "Faltan datos para crear el usuario");
+    const SQLQuery = {
+      text: "INSERT INTO usuarios (nombre, rut, curso, nivel, uuid) VALUES ($1, $2, $3, $4, $5) RETURNING *;",
+      values: [nombre, rut, curso, nivel, uid],
+    };
+    const data = await pool.query(SQLQuery);
+
+    console.log("Usuario Creado", data.rows[0]);
+    // console.log(data);
+    res.status(201).json({ message: "Estudiante creado correctamente" });
+  } catch (error) {
+    errorHandler(res, error);
   }
 });
 
-//Update usuario
+//Actualizando usuario
 app.patch("/usuario", async (req, res) => {
-  try{
-    const { nombre, uid } = req.body;
-  const SQLQuery = {
-    text: "UPDATE usuarios SET nombre = $1 WHERE uuid = $2 RETURNING *;",
-    values: [nombre, uid],
-  }
-  const data = await pool.query(SQLQuery);
-  console.log("Registro modficado:", data.rows[0]);
-  res.status(200).json({ message: "Estudiante actualizado correctamente" });
-
-  }catch (error) {
-    res.status(500).json({ error: error.message })
+  try {
+    const { nombre, uuid } = req.body;
+    if (!nombre || !uuid) {
+      throw new MyError(400, "Faltan datos");
     }
-
-  
+    const SQLQuery = {
+      text: "UPDATE usuarios SET nombre = $1 WHERE uuid = $2 RETURNING *;",
+      values: [nombre, uuid],
+    };
+    const data = await pool.query(SQLQuery);
+    console.log("Registro modficado:", data.rows[0]);
+    res.status(200).json({ message: "Estudiante actualizado correctamente" });
+  } catch (error) {
+    errorHandler(res, error);
+  }
 });
 
-//Delete usuario
+//Eliminando usuario
 app.delete("/usuario/:uuid", async (req, res) => {
-
-  try{
+  try {
     const { uuid } = req.params;
-    if(!uuid) throw new MyError (400, 'Debes colocar un parametro UUID')
-    if(uuid.length !== 6) throw new MyError (400, 'Debes colocar un parametro UUID correcto')
-   
-    
-     await pool.query({
-    text:  "DELETE FROM usuarios WHERE uuid = $1",
-    values: [uuid]
-});
-  res.status(200).json({ message: `Estudiante ${uuid} eliminado correctamente` });
 
-  }catch (error) {
-
-    console.log (error.code, error.message);
-    
-    res.status(error.code).json({ message: error.message })
+    if (!uuid) throw new MyError(400, "Debes colocar un parametro UUID");
+    if (uuid.length !== 6)
+      throw new MyError(400, "Debes colocar un parametro UUID correcto");
+    //Query
+    await pool.query({
+      text: "DELETE FROM usuarios WHERE uuid = $1",
+      values: [uuid],
+    });
+    res
+      .status(200)
+      .json({ message: `Estudiante ${uuid} eliminado correctamente` });
+  } catch (error) {
+    errorHandler(res, error);
   }
 });
 
-//buscando usuario por uuid
+//Buscando usuario por uuid
 app.get("/usuario/:uuid", async (req, res) => {
-  try{
+  try {
     const { uuid } = req.params;
-    
-      const SQLQuery ={
+    if (!uuid) throw new MyError(400, "Debes colocar un parametro UUID");
+    if (uuid.length !== 6)
+      throw new MyError(400, "Debes colocar un parametro UUID correcto");
+    //Query
+    const SQLQuery = {
       text: "SELECT * FROM usuarios WHERE uuid = $1",
-      values: [uuid]
-    }
-  const data = await pool.query(SQLQuery);
-  // if(!uuid) throw new MyError(400, 'UUID no existe')
-  console.log(data);
-  res.status(200).json({ count: data.rowCount, items: data.rows });
-  }catch (error) {
-    res.status(500).json({ error: error.message })
-    console.log({ error: error.message })
+      values: [uuid],
+    };
+    const data = await pool.query(SQLQuery);
+    console.log(data.rows[0]);
+    res.status(200).json({ count: data.rowCount, items: data.rows });
+  } catch (error) {
+    errorHandler(res, error);
   }
 });
 
